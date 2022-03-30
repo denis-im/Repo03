b6b495d46c15e91a8cc547a606cccd62664b5e7d
zabec <denis.imsirovic@gmail.com>
2022-03-30 09:41:01 +0200

    Created ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  // Only change code below this line
+  // My first one line comment
+  /* My first multi line comment
+   */
+  // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
